b4cc575b211273f2dc8c5bfca830d4cbb67cc106
curyPD <romandruzhinin2003@gmail.com>
2022-09-06 17:32:24 +0300

    Implemented image upload

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,43 +1,69 @@
-import React, { useRef } from "react";
+import React, { useState, useRef } from "react";
 import { Editor } from "@tinymce/tinymce-react";
 
-function Dashboard() {
-  /*
-        ACTION PLAN
+import { app } from "../firebase";
+import {
+  getStorage,
+  ref,
+  getDownloadURL,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { async } from "@firebase/util";
 
-        Two options: 
-         - Add New Post
-         - Edit Existing Post
-        
-        ADD NEW POST:
-         1. Display text editor
-         2. Display ADD button
-         3. Add new post to DB
-         4. Display success message 🎉
-         
-        EDIT EXISTING POST:
-         1. Display titles of all existing posts
-         2. Get selected post from DB
-         3. Display text editor with the post text
-         4. After changes have been made, replace contents
-            of the post in DB with new contents. Persist
-            all the other data
-         5. Display success message 🎉
-        
-    */
+const storage = getStorage(app);
+
+function Dashboard() {
+  const [image, setImage] = useState("");
+  const [percent, setPercent] = useState(0);
+  const [imageMessage, setImageMessage] = useState("");
+
   const editorRef = useRef(null);
   const log = () => {
     if (editorRef.current) {
       console.log(editorRef.current.getContent());
     }
   };
+
+  function handleFileSelect(e) {
+    setImageMessage("");
+    const img = e.target.files[0];
+    console.log(img, e.target.value);
+    setImage(img);
+  }
+
+  function handleUpload() {
+    const storageRef = ref(storage, `/files/${image.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+    uploadTask.on(
+      "state_changed",
+      snapshot => {
+        const percent = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+
+        // update progress
+        setPercent(percent);
+      },
+      err => console.error(err),
+      async () => {
+        // download url
+        const url = await getDownloadURL(uploadTask.snapshot.ref);
+        console.log(url);
+        setImageMessage(
+          `Image has been successfully uploaded!
+          URL: ${url}`
+        );
+      }
+    );
+  }
+
   return (
     <>
       <section className="py-10">
         <div className="max-w-screen-xl px-3 sm:px-6 md:px-12 lg:mx-auto xl:px-4">
           <div className="max-w-sm rounded-md bg-white px-7 py-8 shadow-md sm:max-w-md md:max-w-lg lg:mt-16">
-            <h1 className="mb-4 font-sans text-2xl font-normal text-blue-900 sm:text-3xl md:text-4xl md:leading-tight">
-              Hey, Sexy
+            <h1 className="mb-4 font-sans text-2xl font-bold text-blue-900 sm:text-3xl md:text-4xl md:leading-tight">
+              Let's write a new article
             </h1>
             <p className="mb-6 font-serif text-sm text-gray-600 sm:text-base lg:text-lg">
               Have an idea for a new article, or want to edit an existing one?
@@ -53,10 +79,54 @@ function Dashboard() {
           </div>
         </div>
       </section>
-      <section className="bg-white py-8 md:py-10">
-        <h2 className="mb-6 text-center text-xl text-blue-900 md:mb-10 md:text-3xl">
+      <section className="bg-white py-8 md:py-14">
+        <h2 className="mb-6 text-center text-xl font-bold text-blue-900 md:mb-12 md:text-3xl">
           Write an Article
         </h2>
+        <div className="mx-auto mb-10 px-4 sm:max-w-2xl sm:px-8 lg:max-w-4xl lg:px-0">
+          <div className="rounded-md border border-gray-300 px-6 py-3">
+            <h5 className="mb-3 text-center font-serif text-lg font-medium text-gray-700">
+              Add an image
+            </h5>
+            <div>
+              <label
+                htmlFor="file-input"
+                className=" inline-block cursor-pointer rounded-md bg-blue-900 py-2 px-3 font-serif text-sm font-medium text-white transition duration-200 hover:bg-blue-700 sm:px-5 md:text-base"
+              >
+                Select file
+              </label>
+              <input
+                type="file"
+                className="hidden"
+                id="file-input"
+                onChange={handleFileSelect}
+              />
+              {image && (
+                <p className="mt-3 font-serif text-xs text-gray-600">
+                  {image.name}
+                </p>
+              )}
+            </div>
+            {image && (
+              <>
+                <button
+                  onClick={handleUpload}
+                  className="mt-5 rounded-md bg-blue-900 py-2 px-3 font-serif text-sm font-medium text-white transition duration-200 hover:bg-blue-700 sm:px-5 md:text-base"
+                >
+                  Upload
+                </button>
+                <p className="mt-4 font-serif text-sm text-gray-600">
+                  {percent}%
+                </p>
+              </>
+            )}
+            {imageMessage && (
+              <p className="mt-4 font-serif text-xs text-gray-600">
+                {imageMessage}
+              </p>
+            )}
+          </div>
+        </div>
         <div className="mx-auto px-4 sm:max-w-2xl sm:px-8 lg:max-w-4xl lg:px-0">
           <Editor
             apiKey={process.env.REACT_APP_TINY_API_KEY}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,12 +10,12 @@ function Home() {
       <section className="bg-blue-50 py-10 sm:py-16 xl:py-20">
         <div className="flex max-w-screen-xl flex-col gap-12 px-3 sm:px-6 md:px-12 lg:mx-auto lg:flex-row lg:items-start lg:justify-between xl:px-4">
           <div className="max-w-sm rounded-md bg-white px-7 py-8 shadow-md md:max-w-lg lg:mt-16 lg:max-w-md xl:max-w-lg">
-            <h1 className="mb-5 font-sans text-2xl font-normal text-blue-900 sm:text-3xl md:text-4xl md:leading-tight">
+            <h1 className="mb-5 font-sans text-2xl font-bold text-blue-900 sm:text-3xl md:text-4xl md:leading-tight">
               Hop on, it's time to learn a foreign language
             </h1>
             <p className="mb-6 font-serif text-sm text-gray-600 sm:text-base lg:text-lg">
               Hi, my name is Roman, and here I share my language learning
-              journey. If you like learning new things, keep on reading
+              journey. If you like learning new things, keep on reading.
             </p>
             <button className="group flex items-center gap-2 rounded font-serif text-lg font-medium text-blue-900 focus:outline-none focus:ring-2">
               See all articles
@@ -24,7 +24,7 @@ function Home() {
           </div>
 
           <section className="hidden shrink-0 sm:block">
-            <h3 className="mb-8 text-xl text-blue-900 md:text-2xl">
+            <h3 className="mb-8 text-xl font-bold text-blue-900 md:text-2xl">
               Recent Posts
             </h3>
             <RecentCard />
@@ -36,7 +36,7 @@ function Home() {
 
       {/* ARTICLES SECTION */}
       <section className="bg-white py-12 px-3 pb-16 md:py-14 md:px-6 md:pb-24">
-        <h2 className="mb-10 text-center text-2xl text-blue-900 md:mb-12 md:text-3xl">
+        <h2 className="mb-10 text-center text-2xl font-bold text-blue-900 md:mb-12 md:text-3xl">
           Articles
         </h2>
         <div className="mx-auto grid grid-cols-1 items-start justify-items-center gap-x-6 gap-y-12 sm:max-w-2xl sm:grid-cols-2 sm:gap-y-16 md:gap-y-20 lg:max-w-5xl lg:grid-cols-3">
