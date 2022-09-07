c398a65a964ca0ae27b18ee46648b35244300f0d
curyPD <romandruzhinin2003@gmail.com>
2022-09-07 18:27:24 +0300

    redesigned image upload

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Editor } from "@tinymce/tinymce-react";
 
 import { app } from "../firebase";
@@ -8,14 +8,57 @@ import {
   getDownloadURL,
   uploadBytesResumable,
 } from "firebase/storage";
-import { async } from "@firebase/util";
 
 const storage = getStorage(app);
 
 function Dashboard() {
-  const [image, setImage] = useState("");
-  const [percent, setPercent] = useState(0);
-  const [imageMessage, setImageMessage] = useState("");
+  const [title, setTitle] = useState("");
+  const [preface, setPreface] = useState("");
+
+  const [file, setFile] = useState(null);
+  const [imageMessage, setImageMessage] = useState(null);
+  const [fileUrl, setFileUrl] = useState("");
+
+  function handleFileSelect(e) {
+    const img = e.target.files[0];
+    console.log(img, e.target.value);
+    setFile(img);
+  }
+
+  function handleUpload() {
+    const storageRef = ref(storage, `/files/${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      "state_changed",
+      snapshot => {
+        const percent = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setImageMessage(
+          <p className="font-serif text-sm text-gray-600">
+            Upload is <strong className="font-semibold">{percent}</strong>% done
+          </p>
+        );
+      },
+      err => {
+        console.error(err);
+        setImageMessage(
+          <p className="font-serif text-sm text-red-600">
+            Failed to upload image
+          </p>
+        );
+      },
+      async () => {
+        const url = await getDownloadURL(uploadTask.snapshot.ref);
+        setFileUrl(url);
+        setImageMessage(
+          <p className="font-serif text-sm text-green-600">
+            Image successfully uploaded!
+          </p>
+        );
+      }
+    );
+  }
 
   const editorRef = useRef(null);
   const log = () => {
@@ -23,39 +66,6 @@ function Dashboard() {
       console.log(editorRef.current.getContent());
     }
   };
-
-  function handleFileSelect(e) {
-    setImageMessage("");
-    const img = e.target.files[0];
-    console.log(img, e.target.value);
-    setImage(img);
-  }
-
-  function handleUpload() {
-    const storageRef = ref(storage, `/files/${image.name}`);
-    const uploadTask = uploadBytesResumable(storageRef, image);
-    uploadTask.on(
-      "state_changed",
-      snapshot => {
-        const percent = Math.round(
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-        );
-
-        // update progress
-        setPercent(percent);
-      },
-      err => console.error(err),
-      async () => {
-        // download url
-        const url = await getDownloadURL(uploadTask.snapshot.ref);
-        console.log(url);
-        setImageMessage(
-          `Image has been successfully uploaded!
-          URL: ${url}`
-        );
-      }
-    );
-  }
 
   return (
     <>
@@ -81,53 +91,86 @@ function Dashboard() {
       </section>
       <section className="bg-white py-8 md:py-14">
         <h2 className="mb-6 text-center text-xl font-bold text-blue-900 md:mb-12 md:text-3xl">
-          Write an Article
+          Write an article
         </h2>
-        <div className="mx-auto mb-10 px-4 sm:max-w-2xl sm:px-8 lg:max-w-4xl lg:px-0">
-          <div className="rounded-md border border-gray-300 px-6 py-3">
-            <h5 className="mb-3 text-center font-serif text-lg font-medium text-gray-700">
-              Add an image
-            </h5>
-            <div>
-              <label
-                htmlFor="file-input"
-                className=" inline-block cursor-pointer rounded-md bg-blue-900 py-2 px-3 font-serif text-sm font-medium text-white transition duration-200 hover:bg-blue-700 sm:px-5 md:text-base"
-              >
-                Select file
-              </label>
-              <input
-                type="file"
-                className="hidden"
-                id="file-input"
-                onChange={handleFileSelect}
-              />
-              {image && (
-                <p className="mt-3 font-serif text-xs text-gray-600">
-                  {image.name}
+        <section className="lg:max-w-4-xl mx-auto mb-12 grid max-w-sm grid-cols-1 px-4 sm:max-w-lg md:max-w-3xl md:grid-cols-2 md:gap-x-6 md:gap-y-8 lg:gap-x-10">
+          <div className="mb-4 md:mb-0">
+            <label
+              htmlFor="title"
+              className="mb-2 block font-serif text-sm text-gray-700 md:text-base"
+            >
+              Title
+            </label>
+            <input
+              type="text"
+              id="title"
+              className="block w-full rounded-md border border-gray-400 py-2 pl-3 pr-2 font-serif text-sm text-gray-600"
+              name="title"
+              value={title}
+              onChange={e => setTitle(e.target.value)}
+            />
+          </div>
+          <div className="mb-4 md:mb-0">
+            <label
+              htmlFor="preface"
+              className="mb-2 block font-serif text-sm text-gray-700 md:text-base"
+            >
+              Preface
+            </label>
+            <input
+              type="text"
+              id="preface"
+              className="block w-full rounded-md border border-gray-400 py-2 pl-3 pr-2 font-serif text-sm text-gray-600"
+              name="preface"
+              value={preface}
+              onChange={e => setPreface(e.target.value)}
+            />
+          </div>
+          <h5 className="col-span-full row-start-3 row-end-4 mb-2 font-serif text-sm text-gray-700 md:row-start-2 md:row-end-3 md:-mb-2 md:text-base">
+            Add an image
+          </h5>
+          <div className="mb-8 md:mb-0">
+            <label
+              htmlFor="file-input"
+              className=" inline-block cursor-pointer rounded-md bg-blue-900 py-2 px-3 font-serif text-xs font-medium text-white transition duration-200 hover:bg-blue-700 sm:px-5 md:text-base"
+            >
+              Select file
+            </label>
+            <input
+              type="file"
+              className="hidden"
+              id="file-input"
+              onChange={handleFileSelect}
+            />
+            {file && (
+              <div className="mt-5 rounded-md border border-gray-400 py-2 pl-3 pr-2 font-serif text-sm text-gray-600">
+                <p className="truncate font-serif text-sm text-gray-600">
+                  {file.name}
                 </p>
-              )}
-            </div>
-            {image && (
-              <>
-                <button
-                  onClick={handleUpload}
-                  className="mt-5 rounded-md bg-blue-900 py-2 px-3 font-serif text-sm font-medium text-white transition duration-200 hover:bg-blue-700 sm:px-5 md:text-base"
-                >
-                  Upload
-                </button>
-                <p className="mt-4 font-serif text-sm text-gray-600">
-                  {percent}%
-                </p>
-              </>
-            )}
-            {imageMessage && (
-              <p className="mt-4 font-serif text-xs text-gray-600">
-                {imageMessage}
-              </p>
+              </div>
             )}
           </div>
-        </div>
-        <div className="mx-auto px-4 sm:max-w-2xl sm:px-8 lg:max-w-4xl lg:px-0">
+          <div>
+            <div className="flex items-center gap-6">
+              <button
+                disabled={!file}
+                onClick={handleUpload}
+                className="rounded-md bg-blue-900 py-2 px-3 font-serif text-xs font-medium text-white transition duration-200 hover:bg-blue-700 sm:px-5 md:text-base"
+              >
+                Upload
+              </button>
+              {imageMessage}
+            </div>
+            {fileUrl && (
+              <div className="mt-5 rounded-md border border-gray-400 py-2 pl-3 pr-2 font-serif text-sm text-gray-600">
+                <p className="truncate font-serif text-sm text-gray-600">
+                  {fileUrl}
+                </p>
+              </div>
+            )}
+          </div>
+        </section>
+        <section className="mx-auto max-w-sm px-4 sm:max-w-lg md:max-w-2xl lg:max-w-4xl lg:px-0">
           <Editor
             apiKey={process.env.REACT_APP_TINY_API_KEY}
             onInit={(evt, editor) => (editorRef.current = editor)}
@@ -165,7 +208,7 @@ function Dashboard() {
             }}
           />
           <button onClick={log}>Log editor content</button>
-        </div>
+        </section>
       </section>
     </>
   );
