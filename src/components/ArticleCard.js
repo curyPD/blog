c62945b844935b72327103e5b1bc5b7f7e799ea2
curyPD <romandruzhinin2003@gmail.com>
2022-09-09 11:26:50 +0300

    Edit article cards

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -3,26 +3,24 @@ import image from "../img/aaron-burden-QJDzYT_K8Xg-unsplash.jpg";
 import { HiOutlineClock } from "react-icons/hi";
 import { Link } from "react-router-dom";
 
-function ArticleCard(props) {
+function ArticleCard({ data }) {
   return (
-    <article className="w-72 overflow-hidden rounded-md bg-white shadow-lg">
+    <article className="overflow-hidden rounded-md bg-white shadow-lg">
       <Link to="/">
         <div className="relative overflow-hidden">
           <img
-            src={image}
+            src={data.imageUrl}
             alt=""
             className="h-48 w-full object-cover transition-transform duration-300 hover:scale-105"
           />
-          {/* <div className="absolute top-0 left-0 h-full w-full bg-white opacity-0 transition-opacity duration-200 group-hover:opacity-10"></div> */}
         </div>
       </Link>
       <div className="p-5 pt-6">
-        <h4 className="mb-3 font-sans text-lg font-bold leading-tight text-gray-700 md:text-xl md:leading-tight">
-          How to learn a language in 6 months
+        <h4 className="mb-3 font-sans text-lg font-bold leading-tight text-gray-800 md:text-xl md:leading-tight">
+          {data.title}
         </h4>
         <p className="mb-6 font-serif text-sm leading-normal text-gray-500 md:text-base md:leading-normal">
-          You might have seen a title like that somewhere on Youtube, but are
-          you sure it's the right question to ask?
+          {data.preface}
         </p>
         <footer className="flex items-center gap-4">
           <p className="font-serif text-xs text-gray-400">Oct 12, 2022</p>
--- src/components/RecentCard.js
+++ src/components/RecentCard.js
@@ -3,15 +3,15 @@ import image from "../img/aaron-burden-QJDzYT_K8Xg-unsplash.jpg";
 import { HiOutlineClock } from "react-icons/hi";
 import { Link } from "react-router-dom";
 
-function RecentCard(props) {
+function RecentCard({ data }) {
   return (
     <article className="mb-6 flex max-w-md gap-6 rounded-md bg-white p-4 pr-8 shadow-sm last:mb-0">
-      <div className="shrink-0 overflow-hidden rounded-md">
-        <img src={image} alt="" className="h-24 w-auto object-cover" />
+      <div className="w-32 shrink-0 overflow-hidden rounded-md">
+        <img src={data.imageUrl} alt="" className="h-24 w-full object-cover" />
       </div>
       <div className="flex flex-col justify-between">
-        <h4 className="mt-1 cursor-pointer font-sans text-base font-bold leading-snug text-gray-700 transition-colors duration-200 hover:text-blue-700 md:text-lg md:leading-snug">
-          <Link to="/">How to learn a language in 6 months</Link>
+        <h4 className="mt-1 cursor-pointer font-sans text-base font-semibold leading-tight text-gray-800 transition-colors duration-200 hover:text-blue-700 md:text-lg md:leading-snug">
+          <Link to="/">{data.title}</Link>
         </h4>
         <div className="flex items-center gap-4">
           <p className="font-serif text-xs text-gray-400">Oct 12, 2022</p>
